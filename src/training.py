b8b11a9bb89f3474a3a05615855a7752a5e4db18
Maximilian Rüdiger Springenberg <springenberg@campus.tu-berlin.de>
2020-11-18 16:13:18 +0100

    sys paht for training script

--- src/training.py
+++ src/training.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 from argparse import ArgumentParser
 from src.model import *
 from src.data import *
