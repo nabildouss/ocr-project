0d25e5420ef8181103e93951727eac28868a037b
mspringe <mspringenberg@googlemail.com>
2020-11-29 19:59:05 +0100

    latest baseline version

---
+++ src/baseline.py
@@ -0,0 +1,131 @@
+"""
+baseline examples
+"""
+import torch
+from torch import nn
+from src.sliding_window import SlidingWindow, AutomaticWindow
+
+
+class BaseLine1(nn.Module):
+    """
+    This Model uses a LSTM to work directly on the images columns.
+    Jacob recommended this.
+    """
+
+    def __init__(self, shape_in=(1, 32, 256), n_char_class=262):
+        super().__init__()
+        self.shape_in = shape_in
+        self.n_char_class = n_char_class
+        # in this approach the sequence length is defined by the width of the image
+        self.sequence_length = shape_in[-1]
+        # a 3 layer bi-LSTM with dopout allowed
+        self.lstm = nn.LSTM(input_size=self.shape_in[-2], hidden_size=n_char_class, # output
+                            num_layers=3, bidirectional=True, dropout=0.25, # layer specs
+                            batch_first=True) # allowing input of shape (N, T, ...)
+        # as the LSTM is bidirectional, we get twice the ouput -> Linear layer condenses outputs
+        self.out = nn.Linear(2*n_char_class, n_char_class)
+
+    def forward(self, x):
+        # x is of shape (N, 1, F, T), with: N = batch size, F = coulm-wise pixels/ features, T = sequence length
+        y = x.transpose(3,2) # (N, 1, T, F)
+        y = y.squeeze() # (N, T, F)
+        # getting the lstms output
+        y, _ = self.lstm(y) # (N, T, 2*C), where C = number of char. classes
+        y = self.out(y) # (N, T, C)
+        # PyTorchs CTCLoss want input of shape (T, N, C) with a log-softmax activation
+        y = y.permute(1, 0, 2)
+        y = nn.functional.log_softmax(y, dim=2)
+        return y
+
+
+class BaseLine2(nn.Module):
+    """
+    This model uses a CNN to extract features and a lstm to work on columns of the feature maps.
+    """
+
+    def __init__(self, shape_in=(1, 32, 256*4), n_char_class=262):
+        super().__init__()
+        # CNN layer specs
+        conv_layer = lambda c_in, c_out: nn.Sequential(nn.Conv2d(c_in, c_out, kernel_size=(3,3), padding=1), nn.ReLU(),
+                                                       nn.BatchNorm2d(c_out))
+        self.shape_in = shape_in
+        self.n_char_class = n_char_class
+        # The CNN is set up to pool the width, not meddeling with the columns
+        self.cnn = nn.Sequential(conv_layer(self.shape_in[0], 32), nn.MaxPool2d(kernel_size=(1,2)),
+                                 conv_layer(32, 32), nn.MaxPool2d(kernel_size=(1,2)))
+        # as we pool twice, the sequence length is a quater of the images width
+        self.sequence_length = int(self.shape_in[-1]/4)
+        # a 3 layer bi-LSTM with dopout allowed
+        self.lstm = nn.LSTM(input_size=self.shape_in[-2]*32, hidden_size=n_char_class, # output
+                            num_layers=3, bidirectional=True, dropout=0.25, # layer specs
+                            batch_first=True) # allowing input of shape (N, T, ...)
+        # as the LSTM is bidirectional, we get twice the ouput -> Linear layer condenses outputs
+        self.out = nn.Linear(2*n_char_class, n_char_class)
+
+    def forward(self, x):
+        # x is of shape (N, 1, F, 4*T), with: N = batch size, F = coulm-wise pixels/ features, T = sequence length
+        # extracting features/ building feature maps
+        y = self.cnn(x) # (N, 32, F, T)
+        y = y.transpose(3,2) # (N, 32, T, F)
+        bs = [y[:,i] for i in range(y.shape[1])] # (32, N, T, F)
+        y = torch.cat(bs, dim=2) # (N, T, 32*F)
+        # getting the lstms output
+        y, _ = self.lstm(y) # (N, T, 2*C)
+        y = self.out(y) # (N, T, C)
+        # PyTorchs CTCLoss want input of shape (T, N, C) with a log-softmax activation
+        y = y.permute(1, 0, 2)
+        y = nn.functional.log_softmax(y, dim=2)
+        return y
+
+
+class BaseLine3(nn.Module):
+    """
+    This model divides the image into T disjoint sequence-windows.
+    A CNN is used to extract features for each window. (same CNN for each window)
+    The features are then handed to a LSTM
+    """
+
+    def __init__(self, shape_in=(1, 64, 3000), sequence_length=150, n_char_class=262):
+        super().__init__()
+        # CNN layer specs
+        conv_layer = lambda c_in, c_out: nn.Sequential(nn.Conv2d(c_in, c_out, kernel_size=(3,3), padding=1), nn.ReLU(),
+                                                       nn.BatchNorm2d(c_out))
+        self.shape_in = shape_in
+        self.n_char_class = n_char_class
+        self.sequence_length = sequence_length
+        # the CNN
+        n_features = 128
+        assert shape_in[-1] % self.sequence_length == 0, f'width aught to be divisible by {self.sequence_length}'
+        self.stride = int(shape_in[-1] / self.sequence_length)
+        self.cnn = nn.Sequential(conv_layer(self.shape_in[0], 32), nn.MaxPool2d(kernel_size=(2,2)),
+                                 conv_layer(32, 32), nn.MaxPool2d(kernel_size=(2,2)))
+        # dense layer to reduce to a fixed output vector length, e.g. length 128
+        self.fc = nn.Sequential(nn.Linear(32 * int(self.stride/4 * shape_in[-2]/4), 128), nn.ReLU())
+        # as we pool twice, the sequence length is a quater of the images width
+        self.sequence_length = sequence_length
+        # a 3 layer bi-LSTM with dopout allowed
+        self.lstm = nn.LSTM(input_size=n_features, hidden_size=n_char_class, # output
+                            num_layers=3, bidirectional=True, dropout=0.25, # layer specs
+                            batch_first=True) # allowing input of shape (N, T, ...)
+        # as the LSTM is bidirectional, we get twice the ouput -> Linear layer condenses outputs
+        self.out = nn.Linear(2*n_char_class, n_char_class)
+
+    def forward(self, x):
+        # extracting features of sliding windows via CNN
+        y = []
+        for img in x:
+            windows = torch.stack([img[:,:,i*self.stride:i*self.stride+self.stride] 
+                                   for i in range(self.sequence_length)]) # (T, 1, 32, 32)
+            y_i = self.cnn(windows) # (T, 32, H/4, W/4)
+            y_i = torch.flatten(y_i, start_dim=1) # (T, 32 * H/4 * W/4)
+            y_i = self.fc(y_i) # (T, 128)
+            y.append(y_i)
+        # LSTM prob. prediction
+        y = torch.stack(y) # (N, T, 128)
+        y, _ = self.lstm(y) # (N, T, 2*C)
+        y = self.out(y) # (N, T, C)
+        # converting to CTCLoss input shape
+        y = y.permute(1, 0, 2) # (T, N, C)
+        y = torch.log_softmax(y, dim=2)
+        return y
+
--- src/training.py
+++ src/training.py
@@ -7,6 +7,7 @@ from argparse import ArgumentParser
 from src.model import *
 from src import ctc_decoder
 from src.data import *
+from src.baseline import *
 import src.milestone1 as ms1
 import torch
 from torch.utils.data import DataLoader
@@ -29,13 +30,11 @@ class Trainer:
         self.out = out
 
     def crierion(self):
-        return torch.nn.CTCLoss(blank=0, reduction='mean', zero_infinity=True).to(self.device)#, zero_infinity=True)
-        #return torch.nn.L1Loss()#MSELoss()#CTCLoss(blank=0).to(self.device)#, zero_infinity=True)
+        return torch.nn.CTCLoss(blank=0, reduction='mean').to(self.device)
 
     def optimizer(self):
-        return torch.optim.Adam(self.model.parameters())#, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)#lr=1e-4, betas=(0.9, 0.99), weight_decay=0.00005)
-        #return torch.optim.Adam(self.model.parameters(), lr=1e-5, betas=(0.9, 0.99), weight_decay=0.00005)
-        #return torch.optim.SGD(self.model.parameters(),  lr=0.0001, momentum=0.9)#
+        return torch.optim.Adam(self.model.parameters())#, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
+        #return torch.optim.SGD(self.model.parameters(),  lr=0.0001, momentum=0.9)
 
     def train(self):
         # setting up the training:
@@ -51,20 +50,20 @@ class Trainer:
         epoch_count = 1
         last10loss = np.zeros(50)
         while it_count < self.iterations:
-            # new data loader required after each epoch
+            # new data loader initialization required after each epoch 
             dloader = DataLoader(self.dset, batch_size=self.s_batch, num_workers=self.n_workers, shuffle=True,
                                  collate_fn=self.dset.batch_transform)
+            # epoch loop
             for batch, targets, l_targets in dloader:
                 # forward pass
                 batch, targets = batch.to(self.device), targets.to(self.device)
                 y = self.model(batch)
                 # computing loss and gradients
                 loss = criterion(y, targets, L_IN[:len(l_targets)], l_targets)
-                #loss = criterion(y, embds)
                 loss.backward()
                 # optimization step
                 optimizer.step()
-                # finally increasing the optimization step counter
+                # fetching a sample and outputting the best path searchs result (should show if overfitting is happening)
                 if it_count % 100 == 0:
                     hyp = ctc_decoder.decode(y[:,0,:].detach().cpu().numpy())
                     gt_transcript = targets[:l_targets[0]].detach().cpu().numpy()
@@ -76,10 +75,12 @@ class Trainer:
                     print(gt_transcript[:50])
                     print('')
                     print(f'hypthesis: "{self.dset.embedding_to_word(hyp)}"\ngt: "{self.dset.embedding_to_word(gt_transcript)}"')
+                # finally increasing the optimization step counter
                 it_count += 1
                 if it_count >= self.iterations:
                     break
-                if it_count % 1000 == 0:
+                # saving the model every 10K iterations
+                if it_count % 10000 == 0:
                     torch.save(self.model.state_dict(), self.out)
                 if self.prog_bar:
                     prog_bar.update(1)
@@ -87,6 +88,7 @@ class Trainer:
                     last10loss[0] = loss.item()
                     mean_loss = np.mean(last10loss)
                     prog_bar.set_description("epoch %d | mean loss = %f" % (epoch_count, mean_loss))
+            # keeping track of the number of epochs
             epoch_count += 1
         # moving clearing the GPU memory
         batch.cpu()
@@ -105,22 +107,73 @@ def arg_parser():
     return ap
 
 
-def run_training(iterations, data_set, batch_size, device, out, prog_bar, seq_len=132, pixels=32):
+def run_training_1(iterations, data_set, batch_size, device, out, prog_bar, seq_len=256, pixels=32):#seq_len=132
+    # sanity checking for the output path
     if out is not None:
         if not os.path.isdir(os.path.dirname(out)):
             os.makedirs(os.path.dirname(out))
+    # gathering the training data
     train, _ = ms1.load_data(data_set, n_train=0.75, n_test=0.25,
-                             transformation=Compose([Resize([pixels,pixels*seq_len]), ToTensor()]), corpora=[Corpus.EarlyModernLatin],
-                             alphabet=None)#string.ascii_letters + string.digits + ' ')
-    model = BaseLine(n_char_class=len(train.character_classes)+1, sequence_length=seq_len,
-                     shape_in=(1, pixels, pixels))
+                             transformation=Compose([Resize([pixels,seq_len]), ToTensor()]),
+                             corpora=[Corpus.EarlyModernLatin])
+    # setting up the (baseline-) model
+    model = BaseLine1(n_char_class=len(train.character_classes)+1, shape_in=(1, pixels, seq_len))
+    # initializing training loop
     trainer = Trainer(model, train, iterations=iterations, s_batch=batch_size, device=device,
                       prog_bar=prog_bar, out=out)
+    # training
     trainer.train()
+    # savong the trained model
+    torch.save(trainer.model.state_dict(), out)
+
+
+def run_training_2(iterations, data_set, batch_size, device, out, prog_bar, seq_len=256, pixels=32):#seq_len=132
+    # sanity checking for the output path
+    if out is not None:
+        if not os.path.isdir(os.path.dirname(out)):
+            os.makedirs(os.path.dirname(out))
+    # gathering the training data
+    train, _ = ms1.load_data(data_set, n_train=0.75, n_test=0.25,
+                             transformation=Compose([Resize([pixels,4*seq_len]), ToTensor()]),
+                             corpora=[Corpus.EarlyModernLatin])
+    # setting up the (baseline-) model
+    model = BaseLine2(n_char_class=len(train.character_classes)+1, shape_in=(1, pixels, 4*seq_len))
+    # initializing training loop
+    trainer = Trainer(model, train, iterations=iterations, s_batch=batch_size, device=device,
+                      prog_bar=prog_bar, out=out)
+    # training
+    trainer.train()
+    # savong the trained model
+    torch.save(trainer.model.state_dict(), out)
+
+
+def run_training_3(iterations, data_set, batch_size, device, out, prog_bar, seq_len=256, pixels=32):#seq_len=132
+    # sanity checking for the output path
+    if out is not None:
+        if not os.path.isdir(os.path.dirname(out)):
+            os.makedirs(os.path.dirname(out))
+    # gathering the training data
+    train, _ = ms1.load_data(data_set, n_train=0.75, n_test=0.25,
+                             transformation=Compose([Resize([pixels,pixels*seq_len]), ToTensor()]),
+                             corpora=[Corpus.EarlyModernLatin])
+    # setting up the (baseline-) model
+    model = BaseLine3(n_char_class=len(train.character_classes)+1, shape_in=(1, pixels, pixels*seq_len),
+                      sequence_length=seq_len)
+    # initializing training loop
+    trainer = Trainer(model, train, iterations=iterations, s_batch=batch_size, device=device,
+                      prog_bar=prog_bar, out=out)
+    # training
+    trainer.train()
+    # savong the trained model
     torch.save(trainer.model.state_dict(), out)
 
 
 if __name__ == '__main__':
     ap = arg_parser().parse_args()
-    run_training(iterations=ap.iterations,  data_set=ap.data_set, batch_size=ap.batch_size, device=ap.device,
-                 out=ap.out,  prog_bar=ap.prog_bar)
+    #run_training_1(iterations=ap.iterations,  data_set=ap.data_set, batch_size=ap.batch_size, device=ap.device,
+    #               out=ap.out,  prog_bar=ap.prog_bar)
+    #run_training_2(iterations=ap.iterations,  data_set=ap.data_set, batch_size=ap.batch_size, device=ap.device,
+    #               out=ap.out,  prog_bar=ap.prog_bar)
+    run_training_3(iterations=ap.iterations,  data_set=ap.data_set, batch_size=ap.batch_size, device=ap.device,
+                   out=ap.out,  prog_bar=ap.prog_bar)
+
